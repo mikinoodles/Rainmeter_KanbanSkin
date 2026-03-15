a31a7eca4086f327a8c1da1c1d2860dafebe813f
mikinoodles <230642398+mikinoodles@users.noreply.github.com>
2026-03-15 02:17:57 -0500

    Changed generated content

--- _data-generators/ini_generator.py
+++ _data-generators/ini_generator.py
@@ -1,41 +1,29 @@
-MIN = 21
+MIN = 1
 MAX = 30
 
 for i in range(MIN - 1, MAX + 1):
     print(f"""
     [TaskBg{i}]
     Meter=Shape
-    MeterStyle=StyleCardBackground
-    X=0
-    Y=0
     Hidden=1
     DynamicVariables=1
-    LeftMouseDownAction=[!CommandMeasure MeasureScript "PickUpTask({i})"]
+    Group=Tasks
 
     [TaskSummary{i}]
     Meter=String
-    MeterStyle=StyleCardSummary
-    Text=
-    X=0
-    Y=0
     Hidden=1
     DynamicVariables=1
+    Group=Tasks
 
     [TaskProjectBackground{i}]
     Meter=Shape
-    MeterStyle=StyleCardProjectBackground
-    Shape=Rectangle 0,0,([TaskProject{i}:W] + 10),22,6 | Extend ProjectBgModifiers
-    X=0
-    Y=0
     Hidden=1
     DynamicVariables=1
+    Group=Tasks
 
     [TaskProject{i}]
     Meter=String
-    MeterStyle=StyleCardProject
-    Text=
-    X=0
-    Y=0
     Hidden=1
     DynamicVariables=1
+    Group=Tasks
     """)
